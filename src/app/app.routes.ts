4d9b1f013cfa857794593169b3382fa61e62f390
jeanitaz <jeanzeykoharcode@gmail.com>
2025-07-25 07:34:57 -0500

    hola

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'productos', component: ProductosComponent }
+];
